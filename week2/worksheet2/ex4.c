c320c0de07fcde41611adf9920e19834515eda4a
adamsecunda <mindlesslybusiness@gmail.com>
2023-10-16 12:59:46 +0000

    Work on Week 2 Worksheet 2

--- week2/worksheet2/ex4.c
+++ week2/worksheet2/ex4.c
@@ -4,8 +4,7 @@ int main(){
     int arr[5] = {1,2,3,4,5};
     int pos[5] = {};
     int new[5] = {};
-
-    int temp;
+    
     for (int i = 0; i <5; i++){
         int newIndex = (i+1)%5;
         pos[i] = newIndex;
