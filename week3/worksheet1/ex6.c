410ce2fad4abe3c0cdccfb85304cdeb3205a7416
adamsecunda <mindlesslybusiness@gmail.com>
2023-10-17 15:40:25 +0000

    Made change to Worksheet 1 Ex 6

--- week3/worksheet1/ex6.c
+++ week3/worksheet1/ex6.c
@@ -5,15 +5,12 @@ int main(){
     while (num != -1){
         printf("Enter a number: ");
         scanf("%d", &num);
-        if(num == -1){
-            printf("Terminated.\n");
-            break;
-        }
         if(num >0 && num <100){
             printf("%d is between 0 and 100.\n", num);
         }else{
             printf("%d is not between 0 and 100.\n", num);
         }
     }
+    printf("Terminated\n");
     return 0;
 }
